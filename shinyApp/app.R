79dbd6a539463da20adb5924e1714d997f91fab8
Andrew Davis <ddavis3739@gmail.com>
2019-10-08 12:42:13 +0100

    Update app.R

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -9,41 +9,81 @@
 
 library(shiny)
 
+### define functions
+
+labelMandatory <- function(label) {
+    tagList(
+        label,
+        span("*", class = "mandatory_star")
+    )
+}
+
+humanTime <- function() format(Sys.time(), "%Y%m%d-%H%M%OS")
+
+### list input fields
+
+fieldsMandatory <- c("date", "recorder", "location")
+
+optionalField = c('start_time', 'stop_time', 'ownership', 'gear', 'bait', 'samplersNumber', 'depth', 'amountSampled')
+
+
+
+
+
+appCSS <- ".mandatory_star { color: red; }"
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        # Application title
+        shinyjs::useShinyjs(),
+        shinyjs::inlineCSS(appCSS),
+        titlePanel("Field Sampling Input Form"),
+        
+        div(
+            id = "form",
+            
+            textInput("date", labelMandatory("Name"), ""),
+            textInput("recorder", labelMandatory("Recroder")),
+            textInput("location", labelMandatory("Location")),
+            actionButton("submit", "Submit", class = "btn-primary")
         )
-    )
 )
 
+# text inputs 
+text_demo <- textInput(
+    "fav_band", 
+    "What is your favourite 80s band?"
+)
+
+
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+server = function(input, output, session) {
+    observe({
+        mandatoryFilled <-
+            vapply(fieldsMandatory,
+                   function(x) {
+                       !is.null(input[[x]]) && input[[x]] != ""
+                   },
+                   logical(1))
+        mandatoryFilled <- all(mandatoryFilled)
+        
+        shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
+    })    
 }
+
+
+fieldsAll <- c("name", "favourite_pkg", "used_shiny", "r_num_years", "os_type")
+responsesDir <- file.path("responses")
+epochTime <- function() {
+    as.integer(Sys.time())
+}
+
+formData <- reactive({
+    data <- sapply(fieldsAll, function(x) input[[x]])
+    data <- c(data, timestamp = epochTime())
+    data <- t(data)
+    data
+})
 
 # Run the application 
 shinyApp(ui = ui, server = server)
