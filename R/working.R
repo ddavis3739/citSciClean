367fab05acfaefbfa0ea6903498a552df2577c55
Andrew Davis <ddavis3739@gmail.com>
2019-10-08 11:48:50 +0100

    dataChange

--- R/working.R
+++ R/working.R
@@ -2,4 +2,11 @@
 
 #reef$SpeciesCommon = strsplit(reef$Species, )
 
+
+install.packages('taxize')
+library(taxize)
+
 fish = read.csv('data/fish.csv')
+fishNames = read.csv('data/fish_names.csv')
+trips = read.csv('data/trips.csv')
+
